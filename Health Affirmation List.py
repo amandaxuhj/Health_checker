9d39ebd4cd5bff1711cd8004ca13f9169544030e
amandaxuhj <axu@madeira.org>
2021-12-16 20:39:29 -0500

    BMi and check-in questions put together

--- Health Affirmation List.py
+++ Health Affirmation List.py
@@ -1,1 +1,3 @@
-w
+def health_affirmation()->str:
+    Affirmation = ["Investing in your health is one of the best investments you can make.", "Please take time to care for your body and health!","A healthy body is always the result of healthy thoughts and feelings."]
+    
